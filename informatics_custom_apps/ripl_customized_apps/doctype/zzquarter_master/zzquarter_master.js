6fdbf0fae2868729f0834a320fb3fb9fdc9aaeee
Monil Kamboj <monilkamboj@ranainformatics.com>
2025-10-29 12:43:58 +0530

    Minor Changes

--- informatics_custom_apps/ripl_customized_apps/doctype/zzquarter_master/zzquarter_master.js
+++ informatics_custom_apps/ripl_customized_apps/doctype/zzquarter_master/zzquarter_master.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2025, Monil Kamboj and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("zzQuarter Master", {
-// 	refresh(frm) {
 
-// 	},
-// });
+frappe.ui.form.on('zzQuarter Master', {
+	refresh: function(frm) {
+		frm.set_query("cost_center", function() {
+			return {
+				filters: {
+					"company":frm.doc.company,
+					"is_group":0
+
+				}
+			};
+		});
+	}
+});
