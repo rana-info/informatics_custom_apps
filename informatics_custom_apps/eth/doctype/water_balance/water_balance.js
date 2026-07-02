904ebca7a6d23f9819b144ae853e3791548f2c54
Monil Kamboj <monilkamboj@ranainformatics.com>
2026-07-02 16:43:37 +0530

    feat:Added Water Balance Log Book

---
+++ informatics_custom_apps/eth/doctype/water_balance/water_balance.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Monil Kamboj and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Water Balance", {
+// 	refresh(frm) {
+
+// 	},
+// });
