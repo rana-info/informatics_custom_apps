5ce8cdc7cd899098b6875532e7c9585fe85de665
Sahil Negi <sahilnegi@ranainformatics.com>
2026-07-02 16:20:33 +0530

    page

---
+++ informatics_custom_apps/eth/doctype/cpu_plant_lab_log/cpu_plant_lab_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Monil Kamboj and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("CPU Plant Lab Log", {
+// 	refresh(frm) {
+
+// 	},
+// });
