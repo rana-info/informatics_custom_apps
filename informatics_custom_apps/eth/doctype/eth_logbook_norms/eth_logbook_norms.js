21a06a46abd389d59235f6171f73af7d2354f163
Sahil Negi <sahilnegi@ranainformatics.com>
2026-07-04 16:55:38 +0530

    eth norms

---
+++ informatics_custom_apps/eth/doctype/eth_logbook_norms/eth_logbook_norms.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Monil Kamboj and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("ETH Logbook Norms", {
+// 	refresh(frm) {
+
+// 	},
+// });
