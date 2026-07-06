a54acf507c9aed171d59e302267094bc41e9c92d
Monil Kamboj <monilkamboj@ranainformatics.com>
2026-07-06 18:37:40 +0530

    feat:Added DMR input doctypes

---
+++ informatics_custom_apps/eth/doctype/dmr_technical_lab_parameters/dmr_technical_lab_parameters.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Monil Kamboj and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("DMR Technical Lab Parameters", {
+// 	refresh(frm) {
+
+// 	},
+// });
