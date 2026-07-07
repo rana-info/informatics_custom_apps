e0cc465a7eae445aef0b00a0e95c244d8e62c095
Monil Kamboj <monilkamboj@ranainformatics.com>
2026-07-07 10:52:58 +0530

    fix:Added plant inputs and filters

--- informatics_custom_apps/eth/doctype/dmr_technical_lab_parameters/dmr_technical_lab_parameters.js
+++ informatics_custom_apps/eth/doctype/dmr_technical_lab_parameters/dmr_technical_lab_parameters.js
@@ -1,8 +1,24 @@
 // Copyright (c) 2026, Monil Kamboj and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("DMR Technical Lab Parameters", {
-// 	refresh(frm) {
+frappe.ui.form.on("DMR Technical Lab Parameters", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+    company(frm){
+        frm.set_value("plant", "");
+        frm.set_query("plant", function() {
+            return {
+                filters: {
+                    company: frm.doc.company
+                }
+            };
+        });
+    },
+    date(frm){
+        if(frm.doc.date> frappe.datetime.nowdate()){
+            frappe.msgprint("Date cannot be in the future");
+            frm.set_value("date", frappe.datetime.nowdate());
+        }
+    }
+});
--- informatics_custom_apps/eth/doctype/water_balance_log_book/water_balance_log_book.js
+++ informatics_custom_apps/eth/doctype/water_balance_log_book/water_balance_log_book.js
@@ -1,8 +1,24 @@
 // Copyright (c) 2026, Monil Kamboj and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Water Balance Log Book", {
-// 	refresh(frm) {
+frappe.ui.form.on("Water Balance Log Book", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+    company(frm){
+        frm.set_value("plant", "");
+        frm.set_query("plant", function() {
+            return {
+                filters: {
+                    company: frm.doc.company
+                }
+            };
+        });
+    },
+    date(frm){
+        if(frm.doc.date> frappe.datetime.nowdate()){
+            frappe.msgprint("Date cannot be in the future");
+            frm.set_value("date", frappe.datetime.nowdate());
+        }
+    }
+});
