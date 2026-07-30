df18e86d8b2e789fd513d549d874f17d75fe3135
Sahil Negi <sahilnegi@ranainformatics.com>
2026-07-30 11:26:39 +0530

    report

---
+++ informatics_custom_apps/eth/report/boiler_performance_&_fuel_consumption_report/boiler_performance_&_fuel_consumption_report.js
@@ -0,0 +1,23 @@
+// Copyright (c) 2026, Monil Kamboj and contributors
+// For license information, please see license.txt
+
+
+frappe.query_reports["Boiler Performance & Fuel Consumption Report"] = {
+	filters: [
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_days(frappe.datetime.get_today(), -7),
+			reqd: 1,
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.get_today(),
+			reqd: 1,
+		},
+	],
+};
+ 
