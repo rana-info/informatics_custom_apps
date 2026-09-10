24c72490adc5e1ac2e2ec844f54fb9d1522c90b9
Yash Singh <yashkumar_ho@ranainformatics.com>
2026-09-10 10:24:51 +0530

    fix

--- informatics_custom_apps/hooks.py
+++ informatics_custom_apps/hooks.py
@@ -211,11 +211,11 @@ doc_events["Additional Salary"] = {
 
 
 scheduler_events = {
-"cron": {
-        "0 3 * * *": [
-            "informatics_custom_apps.ripl_customized_apps.leave_allocation_override.run_leave_allocation_method"
-        ]
-    },
+# "cron": {
+#         "0 3 * * *": [
+#             "informatics_custom_apps.ripl_customized_apps.leave_allocation_override.run_leave_allocation_method"
+#         ]
+#     },
 "daily_long": [
 		"informatics_custom_apps.ripl_customized_apps.doctype.zzdeputation_request.zzdeputation_request.attendance_request_created",
         "informatics_custom_apps.ripl_customized_apps.cron_job.auto_close_po_specific_items"
