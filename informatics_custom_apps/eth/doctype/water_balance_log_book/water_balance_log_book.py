8dc216407de7c96a20d46bce03f31efd53bc4d7e
Monil Kamboj <monilkamboj@ranainformatics.com>
2026-09-10 11:17:48 +0530

    fix:added calculations for water balance report

--- informatics_custom_apps/eth/doctype/water_balance_log_book/water_balance_log_book.py
+++ informatics_custom_apps/eth/doctype/water_balance_log_book/water_balance_log_book.py
@@ -9,6 +9,7 @@ class WaterBalanceLogBook(Document):
 	
 	def validate(self):
 		self.validate_water_balance()
+		self.calculate_values()
 	def validate_water_balance(self):
 		exists=frappe.db.exists(
 			"Water Balance Log Book",
@@ -18,3 +19,7 @@ class WaterBalanceLogBook(Document):
 		)	
 		if exists and self.get("__islocal"):
 			frappe.throw("Water Balance Log Book already exists for this date and plant.")
+
+	def calculate_values(self):
+		self.total_effluent_genration = self.total_condensate_genration + self.effluent
+		self.ground_water_extraction = self.float_apqq + self.borewell_2
