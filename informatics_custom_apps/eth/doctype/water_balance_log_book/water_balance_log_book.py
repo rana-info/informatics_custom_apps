dac2b9663e614aee142f8985e82c974766dcdd58
Monil Kamboj <monilkamboj@ranainformatics.com>
2026-09-14 15:03:02 +0530

    fix:fixed zero value save error during runtime

--- informatics_custom_apps/eth/doctype/water_balance_log_book/water_balance_log_book.py
+++ informatics_custom_apps/eth/doctype/water_balance_log_book/water_balance_log_book.py
@@ -3,23 +3,36 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
 
 
 class WaterBalanceLogBook(Document):
-	
-	def validate(self):
-		self.validate_water_balance()
-		self.calculate_values()
-	def validate_water_balance(self):
-		exists=frappe.db.exists(
-			"Water Balance Log Book",
-			{
-				"date": self.date,
-				"plant": self.plant,}
-		)	
-		if exists and self.get("__islocal"):
-			frappe.throw("Water Balance Log Book already exists for this date and plant.")
 
-	def calculate_values(self):
-		self.total_effluent_genration = self.total_condensate_genration + self.effluent
-		self.ground_water_extraction = self.float_apqq + self.borewell_2
+    def validate(self):
+        self.validate_water_balance()
+        self.calculate_values()
+
+    def validate_water_balance(self):
+        exists = frappe.db.exists(
+            "Water Balance Log Book",
+            {
+                "date": self.date,
+                "plant": self.plant,
+            }
+        )
+
+        if exists and self.get("__islocal"):
+            frappe.throw(
+                "Water Balance Log Book already exists for this date and plant."
+            )
+
+    def calculate_values(self):
+        self.total_effluent_genration = (
+            flt(self.total_condensate_genration)
+            + flt(self.effluent)
+        )
+
+        self.ground_water_extraction = (
+            flt(self.float_apqq)
+            + flt(self.borewell_2)
+        )
